44c995b4ecbb2a0d769fe2dbd933168d683bd260
alexandermichels <alexandercm4297@gmail.com>
2020-01-17 22:45:50 -0600

    Updates, removed posts, created posters/, custom full page slides

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,5 +1,5 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlides_no_auto(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -26,4 +26,28 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
   setTimeout(plusSlides, 10000, 1);
+}
+
+function showSlides_no_auto(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function plusSlides_no_auto(n) {
+  showSlides_no_auto(slideIndex += n);
+}
+
+function currentSlide_no_auto(n) {
+  showSlides_no_auto(slideIndex = n);
 }
